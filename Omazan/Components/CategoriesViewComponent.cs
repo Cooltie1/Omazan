cdf74a4923608d58708a93d92b41cffd151a4cc3
Scott Olson <scottolsonofficial@gmail.com>
2022-02-23 11:15:15 -0700

    added view component model and some css adjustments

---
+++ Omazan/Components/CategoriesViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omazan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Omazan.Components
+{
+    public class CategoriesViewComponent : ViewComponent 
+    {
+        private IOmazanRepository _repo { get; set; }
+
+        public CategoriesViewComponent (IOmazanRepository tmp)
+        {
+            _repo = tmp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(categories);
+        }
+    }
+}
