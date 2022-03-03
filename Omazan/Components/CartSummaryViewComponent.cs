fb31be4ad0128d44c93a21c43b8e0389013e0d0c
Scott Olson <scottolsonofficial@gmail.com>
2022-03-02 21:31:17 -0700

    Cart summary added

---
+++ Omazan/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Omazan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Omazan.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket _basket;
+        public CartSummaryViewComponent (Basket cartService)
+        {
+            _basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_basket);
+        }
+    }
+}
